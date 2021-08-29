5a7e649495fa0f5523104d213cd553b8cc890f3b
Stanislav Posidelov <sposidelov@roox.ru>
2021-08-29 14:18:39 +0300

    create button with loader

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,86 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyFirstApp());
+
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyFirstAppState();
+  }
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _isLoading = false;
+  double _progressValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: Text("My first App"),
+          title: Text('My First App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: _isLoading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      LinearProgressIndicator(value: _progressValue),
+                      Text(
+                        '${(_progressValue * 100).round()}%',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  )
+                : Text(
+                    'Press buttons to download',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _isLoading = !_isLoading;
+              _updateProgress();
+            });
+          },
+          child: Icon(Icons.cloud_download),
         ),
       ),
-    ),
-  );
+    );
+  }
+
+  void _updateProgress() {
+    const sec = const Duration(seconds: 1);
+    Timer.periodic(sec, (Timer t) {
+      setState(() {
+        _progressValue += 0.1;
+
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _isLoading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
+  }
 }
